d0ea012ee6259b8428761b6faddc799240f4363c
Dan Okkels Brendstrup <dob@eaaa.dk>
2022-03-05 09:43:23 +0100

    L6: Return mock responses for the "fake" requests

--- lesson-06/service-wookiee/sw.js
+++ lesson-06/service-wookiee/sw.js
@@ -11,3 +11,41 @@ self.addEventListener("fetch", async (event) => {
     });
   }
 });
+
+self.addEventListener("fetch", async (event) => {
+  // Generate a correctly formated date string for the "Date" header
+  const dateString = new Date().toUTCString();
+
+  // Determine the file name of the requested file...
+  const fileName = event.request.url.split("/").pop();
+
+  // ...and respond with a mock response if we're requesting a fake.* file
+  if (fileName === "fake.css") {
+    event.respondWith(
+      new Response("/* CSS! */", {
+        headers: {
+          "Date": dateString,
+          "Content-Type": "text/css",
+        },
+      })
+    );
+  } else if (fileName === "fake.html") {
+    event.respondWith(
+      new Response("<!-- HTML! -->", {
+        headers: {
+          "Date": dateString,
+          "Content-Type": "text/html",
+        },
+      })
+    );
+  } else if (fileName === "fake.json") {
+    event.respondWith(
+      new Response('{ "type": "JSON!" }', {
+        headers: {
+          "Date": dateString,
+          "Content-Type": "application/json",
+        },
+      })
+    );
+  }
+});
