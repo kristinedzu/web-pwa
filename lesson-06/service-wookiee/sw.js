8251de26681ed89a06f56f3884c0773bee172efd
Dan Okkels Brendstrup <dob@eaaa.dk>
2022-03-10 23:30:22 +0100

    L6: Cache resources on SW install + cache-first

--- lesson-06/service-wookiee/sw.js
+++ lesson-06/service-wookiee/sw.js
@@ -1,3 +1,20 @@
+const CACHE_NAME = "v1";
+
+// Cache page resources when SW is installed
+self.addEventListener("install", async (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll([
+        "./index.html",
+        "./chewie.jpg",
+        "./main.js",
+        "./style.css",
+      ]);
+    })
+  );
+});
+
+// Send a message to the page for each fetch request
 self.addEventListener("fetch", async (event) => {
   // Find the client that the fetch request was sent from
   const client = await self.clients.get(event.clientId);
@@ -12,6 +29,7 @@ self.addEventListener("fetch", async (event) => {
   }
 });
 
+// Mock responses to the fake.* file requests
 self.addEventListener("fetch", async (event) => {
   // Generate a correctly formated date string for the "Date" header
   const dateString = new Date().toUTCString();
@@ -48,4 +66,13 @@ self.addEventListener("fetch", async (event) => {
       })
     );
   }
+});
+
+// Cache-first strategy for other resources
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
 });
